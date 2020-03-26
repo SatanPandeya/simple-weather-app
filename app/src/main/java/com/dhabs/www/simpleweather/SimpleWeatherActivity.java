14ec0b8a4988ab9bba02af37344f03d70ff46359
SatanPandeya <satanpandeya@gmail.com>
2020-03-26 12:28:05 +0545

    First commit

---
+++ app/src/main/java/com/dhabs/www/simpleweather/SimpleWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.dhabs.www.simpleweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_weather);
+    }
+}
